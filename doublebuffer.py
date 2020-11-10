a9b3abffea8854a672e9c092d9e864a0c8ce98cd
Sunjay Cauligi <scauligi@eng.ucsd.edu>
2020-11-10 12:05:00 -0800

    doublebuffer.py options to wrap cmd with script and/or bash -i

--- doublebuffer.py
+++ doublebuffer.py
@@ -13,7 +13,17 @@ CLEAR_TERM = '\033[H'
 def blen(line):
     return len(re.sub('\033.*?m', '', line))
 
-def do(cmd, seconds):
+def quote(s):
+    s = s.replace('\\', '\\\\')
+    s = s.replace('"', '\\"')
+    return s
+
+def do(args):
+    cmd = args.command
+    if args.interactive_shell:
+        cmd = f'bash -ic "{quote(cmd)}"'
+    if args.pseudo_terminal:
+        cmd = f'script -qec "{quote(cmd)}" /dev/null'
     while True:
         termsize = os.get_terminal_size()
         fillout = termsize.columns
@@ -40,15 +50,17 @@ def do(cmd, seconds):
         print('\n'.join(lines))
         for i in range(linecount, termsize.lines - 1):
             print(' ' * termsize.columns)
-        time.sleep(seconds)
+        time.sleep(args.interval)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-n', '--interval', metavar='seconds', action='store', type=int, default=5, help='seconds to wait between updates')
+    parser.add_argument('-t', '--pseudo-terminal', action='store_true', help='run command using `script` to fake a TTY')
+    parser.add_argument('-i', '--interactive-shell', action='store_true', help='run command using `bash -i`')
     parser.add_argument('command')
     args = parser.parse_args()
     print(HIDE_CURSOR, end='')
     try:
-        do(args.command, seconds=args.interval)
+        do(args)
     finally:
         print(SHOW_CURSOR, end='', flush=True)
